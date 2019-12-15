46b883afba40346c799d0f52efcfbd9a863714e5
AnnaGhazaryan <58561909+AnnaGhazaryan@users.noreply.github.com>
2019-12-16 00:50:51 +0400

    Create Exercise8.js

---
+++ Exercise8.js
@@ -0,0 +1,18 @@
+// Exercise 8
+
+  for (var i =1; i<=12; i++){
+    console.log(i);
+if (i==3){
+  console.log("Winter");
+} else
+if (i == 6) {
+   console.log("Spring");
+} else
+if (i == 9) {
+   console.log("Summer");
+}
+else
+if (i == 12) {
+   console.log("Autumn");
+}
+  }
